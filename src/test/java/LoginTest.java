b08c204242d8c990d09ec29e84354d926a744b80
bhaskararao pedinedi <bhaskararao pedinedi>
2017-02-16 11:11:34 -0600

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+public static WebDriver driver;
+@BeforeTest	 
+public void setup(){
+		 System.setProperty("webdriver.gecko.driver","C:\\Users\\User\\Downloads\\geckodriver-v0.11.1-win64\\geckodriver.exe");
+		  driver = new FirefoxDriver();
+	 }
+@Test	 
+public void donavigate(){
+		 driver.get("http://www.google.com");
+			
+	     driver.navigate().to("http://www.seleniumhq.org/"); 
+	        System.out.println(driver.getCurrentUrl());		 
+	 }
+@AfterTest
+public void teardown(){
+	driver.quit();
+	}
+}
